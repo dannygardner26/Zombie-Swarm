b1de0da5050b48a5cad70d0e8be1cf1ab3fef38b
mdas26 <143539335+mdas26@users.noreply.github.com>
2024-05-22 13:36:41 -0400

    modified:   Zombie Swarm/gun.java

--- Zombie Swarm/gun.java
+++ Zombie Swarm/gun.java
@@ -1,4 +1,24 @@
-public class Gun{
+import javax.swing.ImageIcon;
+
+public class Gun extends GameObject {
+
+    private ImageIcon[][] iconGun;
+    public Gun(int x, int y){
+        super(x,y);
+        this.setSize(15,20);
+
+        iconGun = new ImageIcon[4][5];
+
+        for(int i = 0; i<iconGun.length; i++){
+            for(int j= 0; j<iconGun[0].length; j++){
+                if(i==3 && j==4){
+                    iconGun[i][j]= null;
+                }
+                iconGun[i][j]= new ImageIcon("./images/")
+            }
+        }
+    }
+
 
     
     
