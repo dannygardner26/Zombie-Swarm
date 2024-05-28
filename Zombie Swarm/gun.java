0ed72f54da6b8a446c1fef2769d890f524b3a6e7
dannygardner26 <99089784+dannygardner26@users.noreply.github.com>
2024-05-28 12:53:56 -0400

    modified:   Zombie Swarm/gun.java

--- Zombie Swarm/gun.java
+++ Zombie Swarm/gun.java
@@ -3,13 +3,22 @@ import javax.swing.ImageIcon;
 public class Gun{
 
     private ImageIcon iconGun;
+    private int damage
+    private int bulletSpeed
+    private int reloadSpeed
+
+
+
+
 
     public Gun(ImageIcon icon, int damage, double bulletSpeed, int reloadSpeed){
         super(x,y);
         this.setSize(15,20);
 
         iconGun = new ImageIcon[4][5];
-
+        this.damage = damage;
+        this.bulletSpeed = bulletSpeed;
+        this.reloadSpeed = reloadSpeed;
         
         
     }
