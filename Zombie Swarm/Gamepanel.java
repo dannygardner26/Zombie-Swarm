96375501e9601c4eda3387c16fc31b842e2262ff
dannygardner26 <99089784+dannygardner26@users.noreply.github.com>
2024-05-30 13:07:57 -0400

    modified:   Zombie Swarm/Gamepanel.java

---
+++ Zombie Swarm/Gamepanel.java
@@ -0,0 +1,170 @@
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * This class will house all game objects which are part of the
+ * simulation. You must add several targets which can be
+ * hit by fireballs and removed from the game.
+ */
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    private BufferedImage background;
+    private Hero hero;
+    private static GamePanel tempGP;
+    private ArrayList<Gun> gunList;
+   
+
+    /**
+     
+     */
+    public GamePanel() {
+        this.setLayout(null);
+        tempGP = this;
+
+        ArrayList<ImageIcon> gunPics = new ArrayList<ImageIcon>();
+
+        for(int i = 0; i < 19; i++){
+            gunPics.add(new ImageIcon("./images/realGun" + i + ".png"));
+        }
+        Gun pistol = new Gun("pistol", 0, 0, gunPics.get(0), 5, 10, 10);
+        gunList.add(pistol);
+        Gun AssaultRifle = new Gun("pistol", 0, 0, gunPics.get(1), 5, 10, 10);
+        gunList.add(pistol);
+
+        hero = new Hero(100, 100, tempGP, gunList);
+        this.add(hero);
+        hero.setVisible(true);
+        
+        
+
+
+        URL imageURL = getClass().getResource("./images/Background.jpg");
+
+        try {
+            background = ImageIO.read(imageURL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<ImageIcon> gunPics = new ArrayList<ImageIcon>();
+
+        for(int i = 0; i < 19; i++){
+            gunPics.add(new ImageIcon("./images/realGun" + i + ".png"));
+        }
+        Gun pistol = new Gun("pistol", 0, 0, gunPics.get(0), 5, 10, 10);
+        gunList.add(pistol);
+        Gun AssaultRifle = new Gun("pistol", 0, 0, gunPics.get(1), 5, 10, 10);
+        gunList.add(pistol);
+
+        
+    
+        // Handle key presses. We will set dx/dy for the character
+        this.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                
+
+
+                int code = e.getKeyCode();
+                switch (code) {
+                    case KeyEvent.VK_W:
+                            hero.setDy(-5);
+                            hero.setDx(0); 
+                        hero.setDirection(new Direction(Direction.UP));                     
+                        break;
+                    case KeyEvent.VK_A:
+                            hero.setDy(0);
+                            hero.setDx(-5);
+                        hero.setDirection(new Direction(Direction.LEFT));
+                        break;
+                    case KeyEvent.VK_S:                       
+                            hero.setDy(5);
+                            hero.setDx(0);                      
+                        hero.setDirection(new Direction(Direction.DOWN));
+                        break;
+                    case KeyEvent.VK_D:
+                            hero.setDy(0);// moving up
+                            hero.setDx(5);// horizontal v Vertical mutually exclusive                              
+                        hero.setDirection(new Direction(Direction.RIGHT));
+                        break;
+
+                    case KeyEvent.VK_SPACE:
+                        
+                    
+                        break;
+                             
+                    
+                }
+            }
+            
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                int code = e.getKeyCode();
+                switch (code) {
+                    case KeyEvent.VK_W:
+                        
+                    case KeyEvent.VK_A:
+                        hero.setDx(0);
+                        hero.setIdle();
+                        break;
+                    case KeyEvent.VK_S:
+                        hero.setDy(0);
+                        hero.setIdle();
+                        break;
+                    case KeyEvent.VK_D:
+                        hero.setDx(0);
+                        hero.setIdle();
+                        break;
+                }
+            }
+
+        });
+
+        // create and start the game timer. This gamepanel is passed
+        // as the action listener which will be triggered every 17 milliseconds
+        Timer gameLoop = new Timer(17, this);
+        gameLoop.start();
+
+        
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), this);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        hero.update();
+        
+    }
+
+    
+
+    
+
+}
