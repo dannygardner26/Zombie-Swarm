a3ea505a4c190841072fc12d41c33ba753eaa340
mdas26 <143539335+mdas26@users.noreply.github.com>
2024-06-03 21:41:32 -0400

    Update gun.java

--- Zombie Swarm/gun.java
+++ Zombie Swarm/gun.java
@@ -14,7 +14,6 @@ public class Gun extends GameObject{
     private String name;
     private ArrayList<Bullet> bulletList;
     private int ammo;
-    private int bulletSpeed;
 
     public Gun(String name, int x, int y, ImageIcon icon, int damage, int reloadSpeed, int ammo, int fireRate){
         super(x,y);
@@ -23,7 +22,6 @@ public class Gun extends GameObject{
         this.visible = false;
         iconGun = icon;
         this.damage = damage;
-        this.bulletSpeed = bulletSpeed;
         this.reloadSpeed = reloadSpeed;
         this.name = name;
         this.ammo = ammo;
@@ -32,7 +30,8 @@ public class Gun extends GameObject{
     
     @Override
     public void update() {
-        
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
     public void reload(){
@@ -51,6 +50,7 @@ public class Gun extends GameObject{
         return fireRate;
     }
 
+    
     
 
     // public void fire(int x, int y, int mouseX, int mouseY){
