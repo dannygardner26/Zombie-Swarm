fb65487c3bd2ab3040191c3431cd07b6909840f4
mdas26 <143539335+mdas26@users.noreply.github.com>
2024-06-05 12:57:49 -0400

    manas

--- PowerUps.java
+++ PowerUps.java
@@ -1,4 +1,22 @@
-public class PowerUps{
+import java.util.ArrayList;
+
+public class PowerUps extends GameObject
+{
+
+    private ArrayList<String> powerUps;
+    private Hero hero;
+
+    //public PowerUps(int x, int y, Hero hero)
+    //{
+      //  this.hero = hero;
+
+
+  //  }
+
+    public void update(){
+
+    }
+
 
 
     
