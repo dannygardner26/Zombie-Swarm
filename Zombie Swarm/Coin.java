cb74100b649a89add8b15455fd6fa77995ce4081
mdas26 <143539335+mdas26@users.noreply.github.com>
2024-06-05 21:12:00 -0400

    Update Coin.java

--- Zombie Swarm/Coin.java
+++ Zombie Swarm/Coin.java
@@ -1,36 +1,65 @@
 import javax.swing.ImageIcon;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Coin extends GameObject {
 
-    private ImageIcon icon;
+    private ImageIcon[] icons;
     private Hero hero;
     private int coinCount;
     private boolean done;
+    private int phase;
+    private Timer coinTimer;
+    
 
     public Coin(int x, int y, Hero hero) {
         super(x, y);
-        //this.setLocation(x,y);
         this.setSize(16, 16);
-        icon = new ImageIcon("./Zombie Swarm/images/Coinreal.png");
-        this.setIcon(icon);
-        this.hero = hero;
-        this.done = false;
-    }
 
-
-
-    public void update(){
-        boolean collision = hero.hasCollidedWith(this);
-        if(collision){
-            coinCount++;
-            this.setVisible(false);
+        
+        icons = new ImageIcon[6];
+        
+        for (int i = 0; i < 2; i++) {
+            for(int j = 0; j<3; j++){
+                icons[i] = new ImageIcon("./Zombie Swarm/images/coin_" + i +"_" + j +".png");
+            }
+            
         }
 
         
+        phase = 0;
+        this.setIcon(icons[phase]);
+
+        this.hero = hero;
+        this.done = false;
+
+        
+        coinTimer = new Timer(100, new ActionListener() { 
+            
+            public void actionPerformed(ActionEvent e) {
+                animate();
+            }
+        });
+        coinTimer.start();
     }
 
-    public boolean isDone(){
+   
+    private void animate() {
+        phase = (phase + 1) % icons.length;
+        this.setIcon(icons[phase]);
+    }
+
+    public void update() {
+        boolean collision = hero.hasCollidedWith(this);
+        if (collision) {
+            coinCount++;
+            this.setVisible(false);
+            coinTimer.stop();
+        }
+    }
+
+    public boolean isDone() {
         return done;
     }
-
 }
