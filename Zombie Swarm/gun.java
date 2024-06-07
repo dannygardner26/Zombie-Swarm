ce6dedde2d7e1972bdbb5e4552ef7d7281e34251
dannygardner26 <99089784+dannygardner26@users.noreply.github.com>
2024-06-07 13:32:18 -0400

    modified:   Zombie Swarm/gun.java 	modified:   Zombie Swarm/Gun.java

--- Zombie Swarm/gun.java
+++ Zombie Swarm/gun.java
@@ -75,6 +75,10 @@ public class Gun extends GameObject{
         return this.ammo;
     }
 
+    public ImageIcon getPng(){
+        return icon;
+    }
+
     public int getMaxAmmo(){
         return maxAmmo;
     }
